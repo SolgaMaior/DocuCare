f04e882c7914a1566e626a79c7946006b08358d5
Kyle Marion Cenon C. Sabado <kmccsabado23@bpsu.edu.ph>
2025-09-29 17:05:29 +0800

    changed db

--- model/db_con.php
+++ model/db_con.php
@@ -1,11 +1,11 @@
 <?php
 
-$host = "localhost";
-$user = "root";
+$host = "192.168.1.53";
+$user = "docuuser";
 $dbname  = "citizensdb";
 
 try {
-    $db = new PDO("mysql:host=127.0.0.1;port=3307;dbname=$dbname", "$user", "");
+    $db = new PDO("mysql:host=$host;port=3307;dbname=$dbname", "$user", "");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     die("Connection failed: " . $e->getMessage());
