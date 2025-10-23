9ca35fa420337c36e73db7c456f400664e8da3a4
Godson Duhaylungsod <duhaylungsodgodson@gmail.com>
2025-10-23 16:05:04 +0800

    approve accounts

---
+++ view/accounts_approve.view.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles/record.css">
+</head>
+<body>
+    
+<?php
+$current_page = basename($_SERVER['PHP_SELF']);
+require('view/partials/sidebar.php');
+?>
+    
+</body>
+</html>
